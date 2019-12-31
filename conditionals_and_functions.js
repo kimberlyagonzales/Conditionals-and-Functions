eb708134e17275119978b81d213bf96aa62f4f9d
Kimberly Gonzales <58645315+kimberlyagonzales@users.noreply.github.com>
2019-12-31 15:29:41 -0700

    Add files via upload

---
+++ conditionals_and_functions.js
@@ -0,0 +1,16 @@
+/*...*/
+/*...*/
+/*...*/
+
+let x = 10;
+
+function addNumbers (n, m, x) {
+	console.log((x));
+	if(1===1) {
+		let b = 8;
+	}
+	console.log(b);
+	return n + m;
+}
+
+addNumbers(1, 2, 10);
